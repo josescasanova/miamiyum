a8f56807b2e4fd236e22cfd2ff0045f17c4f10e7
Jose Casanova <josescasanova@gmail.com>
2014-04-18 16:46:06 -0400

    got api to work

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,27 +1,7 @@
 class DealsController < ApplicationController
 
-  SqootClient.total_sqoot_deals(location: 'Miami', category_slug: 'restaurants').each do |x|
-    merchant_unique = Merchant.where(name: x.merchant.name).first
-    deal_unique = Deal.where(title: x.title).first
-
-    if merchant_unique != x.merchant.name
-      if x.category_slug == "restaurants"
-        merchant = Merchant.create(name: x.merchant.name, address: x.merchant.address, locality: x.merchant.locality, region: x.merchant.region, postal_code: x.merchant.postal_code, country: x.merchant.country_code, latitude: x.merchant.latitude, longitude: x.merchant.longitude)
-      else
-        merchant = merchant_unique
-      end
-    end
-
-    if deal_unique != x.title
-      if x.category_slug != nil
-        if x.category_slug == "restaurants"
-          deal = Deal.create(title: x.title, category: x.category_slug, deal_url: x.url, image_url: x.image_url, expires_at: x.expires_at, price: x.price, value: x.value, merchant: merchant)
-        end
-      end
-    end
-  end
-
   def index
+    call_sqoot_api
     @deals = Deal.all
     @hash = Gmaps4rails.build_markers(@deals) do |deal, marker|
       marker.lat deal.merchant.latitude
@@ -31,6 +11,31 @@ class DealsController < ApplicationController
         "url" => "http://i.imgur.com/aHTyCas.png",
         "width" => 16,
         "height" => 50})
+    end
+  end
+
+  private
+# http://api.sqoot.com/v2/deals?api_key=l6frg7&location=Miami&radius=20&category_slugs=restaurants
+  def call_sqoot_api
+    SqootClient.deals(location: 'Miami', radius: 5, page: 1, per_page: 50, category_slugs: 'restaurants').each do |x|
+      merchant_unique = Merchant.where(name: x.merchant.name).first
+      deal_unique = Deal.where(title: x.title).first
+
+      if merchant_unique != x.merchant.name
+        if x.category_slug == "restaurants"
+          merchant = Merchant.create(name: x.merchant.name, address: x.merchant.address, locality: x.merchant.locality, region: x.merchant.region, postal_code: x.merchant.postal_code, country: x.merchant.country_code, latitude: x.merchant.latitude, longitude: x.merchant.longitude)
+        else
+          merchant = merchant_unique
+        end
+      end
+
+      if deal_unique != x.title
+        if x.category_slug != nil
+          if x.category_slug == "restaurants"
+            deal = Deal.create(title: x.title, category: x.category_slug, deal_url: x.url, image_url: x.image_url, expires_at: x.expires_at, price: x.price, value: x.value, merchant: merchant)
+          end
+        end
+      end
     end
   end
 
