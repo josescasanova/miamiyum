3095cf50921dac4d4e794a6ed395a0c7805dfbea
Jose Casanova <josescasanova@gmail.com>
2014-04-17 23:20:42 -0400

    added navbar and made map full screen

---
+++ config/initializers/rsqoot_config.rb
@@ -0,0 +1,8 @@
+RSqoot.configure do |config|
+  config.public_api_key        = ENV['SQOOT_PUBLIC_KEY']
+  config.private_api_key       = ENV['SQOOT_PRIVATE_KEY']
+  config.base_api_url          = 'http://api.sqoot.com/v2/'
+  config.authentication_method = :header
+  config.read_timeout          = 60.seconds
+  config.expired_in            = 1.hour
+end
