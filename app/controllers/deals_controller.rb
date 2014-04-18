142b02da4e11b11c1e620c5bd864e1b5d88e8b6e
Jose Casanova <josescasanova@gmail.com>
2014-04-18 14:03:47 -0400

    got the basics of the api working

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,30 +1,22 @@
 class DealsController < ApplicationController
 
+
+  SqootClient.deals(location: 'Miami', categories: 'restaurants', page: 1, per_page: 2).each do |x|
+    merchant = Merchant.create(name: x.merchant.name, address: x.merchant.address, locality: x.merchant.locality, region: x.merchant.region, postal_code: x.merchant.postal_code, country: x.merchant.country_code, latitude: x.merchant.latitude, longitude: x.merchant.longitude)
+    deal = Deal.create(title: x.title, category: x.category_name, deal_url: x.url, image_url: x.image_url, expires_at: x.expires_at, price: x.price, value: x.value, merchant: merchant)
+  end
+
   def index
     @deals = Deal.all
     @hash = Gmaps4rails.build_markers(@deals) do |deal, marker|
       marker.lat deal.merchant.latitude
       marker.lng deal.merchant.longitude
       marker.infowindow deal.title
+      marker.picture({
+        "url" => "http://i.imgur.com/aHTyCas.png",
+        "width" => 16,
+        "height" => 50})
     end
   end
 
-
-  # x = SqootClient.deals(location: 'Miami', categories: 'restaurants', page: 1, per_page: 2).each do |x|
-  #   x.title
-  #   x.category_name
-  #   x.url
-  #   x.image_url
-  #   x.expires_at
-  #   x.price
-  #   x.value
-  #   x.merchant.name
-  #   x.merchant.address
-  #   x.merchant.locality
-  #   x.merchant.region
-  #   x.merchant.postal_code
-  #   x.merchant.country_code
-  #   x.merchant.latitude
-  #   x.merchant.longitude
-  # end
 end
