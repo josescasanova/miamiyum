bba95e917dc410b77cae4e4034cef1d7134eea75
Jose Casanova <josescasanova@gmail.com>
2014-04-18 20:47:26 -0400

    merchant and deal duplication

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -18,22 +18,22 @@ class DealsController < ApplicationController
 # http://api.sqoot.com/v2/deals?api_key=l6frg7&location=Miami&radius=20&category_slugs=restaurants
   def call_sqoot_api
     SqootClient.deals(location: 'Miami', radius: 5, page: 1, per_page: 50, category_slugs: 'restaurants').each do |x|
+      # binding.pry
       merchant_unique = Merchant.where(name: x.merchant.name).first
       deal_unique = Deal.where(title: x.title).first
 
-      if merchant_unique != x.merchant.name
-        if x.category_slug == "restaurants"
-          merchant = Merchant.create(name: x.merchant.name, address: x.merchant.address, locality: x.merchant.locality, region: x.merchant.region, postal_code: x.merchant.postal_code, country: x.merchant.country_code, latitude: x.merchant.latitude, longitude: x.merchant.longitude)
+      if merchant_unique == nil  
+      # (merchant_unique != x.merchant.name) || (merchant_unique == nil) 
+      # figure out how to CREATE a new merchant if merchant_unique is NIL AND IS UNIQUE if nil -> create, if not nil -> check then if unique, create
+        # if x.category_slug == "restaurants"
+        merchant = Merchant.create(name: x.merchant.name, address: x.merchant.address, locality: x.merchant.locality, region: x.merchant.region, postal_code: x.merchant.postal_code, country: x.merchant.country_code, latitude: x.merchant.latitude, longitude: x.merchant.longitude)
         else
           merchant = merchant_unique
-        end
       end
 
-      if deal_unique != x.title
-        if x.category_slug != nil
-          if x.category_slug == "restaurants"
-            deal = Deal.create(title: x.title, category: x.category_slug, deal_url: x.url, image_url: x.image_url, expires_at: x.expires_at, price: x.price, value: x.value, merchant: merchant)
-          end
+      if deal_unique == nil
+        if x.category_slug == "restaurants"
+          deal = Deal.create(title: x.title, category: x.category_slug, deal_url: x.url, image_url: x.image_url, expires_at: x.expires_at, price: x.price, value: x.value, merchant: merchant)
         end
       end
     end
